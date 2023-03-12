82dd66bdc10488ba925f8d1662197d03aa86ee9a
Malaica Brunks <malaica.brunks@gmx.de>
2023-03-12 13:14:23 +0100

    start about-me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainPresentationComponent } from './main-presentation/main-presentation.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainPresentationComponent
+    MainPresentationComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
