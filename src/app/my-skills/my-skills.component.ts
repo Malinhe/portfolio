735beae715d1c12d152f0a4568979ab3e5d5c016
Malaica Brunks <malaica.brunks@gmx.de>
2023-03-16 15:05:26 +0100

    start skill section

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+ 
   selector: 'app-my-skills',
   templateUrl: './my-skills.component.html',
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent {
+ 
 
 }
